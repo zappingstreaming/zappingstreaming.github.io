ebf3cad0ec68b9abaf533edd06e46021075b9ccd
zappingstreaming <pedropirulo6@gmail.com>
2024-07-24 08:17:44 -0300

    frontend appbar improvements

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {"assets/argstreams/argstreams.json": "949f580cf115ee698b6bb8d
 "icons/Icon-512.png": "fda8e6dc0cda9298141134c9457895da",
 "icons/Icon-maskable-192.png": "b2036395a972ee9aadbc6a32e92de93c",
 "icons/Icon-maskable-512.png": "fda8e6dc0cda9298141134c9457895da",
-"index.html": "311f5113f8bc4b295ac596e30595ec26",
-"/": "311f5113f8bc4b295ac596e30595ec26",
-"main.dart.js": "32eb299b327fa410c80550cb079d0618",
+"index.html": "210470e7143c5362b7daa28284ccd30d",
+"/": "210470e7143c5362b7daa28284ccd30d",
+"main.dart.js": "89979a746a7bf73a264552625d4aafcc",
 "manifest.json": "7a8d1eea6913e82677d8f0deb3a1ebe9",
 "version.json": "088f5f76089b6211e96d0fcc5ba088b2"};
 // The application shell files that are downloaded before a service worker can
